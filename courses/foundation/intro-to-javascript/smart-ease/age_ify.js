73645864898c5a1f9238f3f47f096b4e6aef9967
Shipra Srivastava <shiprasrvstv1988@gmail.com>
2025-11-04 22:01:32 +0100

    Revert "Adding intro to javascript week1 assignment"

--- courses/foundation/intro-to-javascript/smart-ease/age_ify.js
+++
@@ -1,8 +0,0 @@
-const yearOfBirth = 1988;
-const yearFuture = 2045;
-
-const age = yearFuture - yearOfBirth;
-
-console.log(
-  "You will be " + age + " years old in the year " + yearFuture + "."
-);
