298e88a4b36cc392cabc7f12b210e5dc56c01594
shpomp <karolina.urnieziute@gmail.com>
2025-12-17 18:24:12 +0100

    230 - -react-week-4-assignment rollback mui recomm

--- courses/frontend/react/space-galactica-app/src/components/Badge.jsx
+++ courses/frontend/react/space-galactica-app/src/components/Badge.jsx
@@ -1,9 +1,5 @@
 import styles from './Badge.module.css';
 
-// This component is a good candidate to be used from a component library.
-// You can look at the MUI Badge component and consider using it:
-// https://mui.com/material-ui/react-badge/
-
 export const Badge = ({ count, children }) => {
   return (
     <div className={styles.badge}>
