81b63eaeeac9ab12c2025ea3243b9615a3fd7b43
Shipra Srivastava <shiprasrvstv1988@gmail.com>
2026-04-01 08:36:37 +0200

    feat: add Tea class with validation and factory method

---
+++ courses/backend/advanced-javascript/week4/exercise1.js
@@ -0,0 +1,63 @@
+import { teas } from "../week1/data/teas.js";
+// Exercise 1: Tea Class with Validation
+
+const typeofTeas = ["green", "black", "herbal", "oolong", "white"];
+export class Tea {
+  constructor(name, type, origin, pricePerGram, organic) {
+    if (!name) {
+      throw new Error("Name is required");
+    } else if (!typeofTeas.includes(type)) {
+      throw new Error(`Invalid type: ${type}`);
+    } else if (pricePerGram < 0) {
+      throw new Error("Price must be positive");
+    } else {
+      this.name = name;
+      this.type = type;
+      this.origin = origin;
+      this.pricePerGram = pricePerGram;
+      this.organic = organic;
+    }
+  }
+
+  priceFor(grams) {
+    return this.pricePerGram * grams;
+  }
+
+  describe() {
+    const price = this.pricePerGram * 100;
+    const organicTag = this.organic ? "[organic]" : "";
+
+    return `${this.name} (${this.type}) from ${this.origin} - ${price} DKK/100g ${organicTag}`;
+  }
+
+  static fromObject(obj) {
+    return new Tea(
+      obj.name,
+      obj.type,
+      obj.origin,
+      obj.pricePerGram,
+      obj.organic
+    );
+  }
+}
+
+// Test validation:
+try {
+  new Tea("", "green", "Japan", 0.12, true);
+} catch (e) {
+  console.log(e.message);
+} // "Name is required"
+
+try {
+  new Tea("Test", "purple", "Japan", 0.12, true);
+} catch (e) {
+  console.log(e.message);
+} // "Invalid type: purple"
+
+// Test factory method:
+const teaInstances = teas.map(Tea.fromObject);
+console.log(teaInstances.length); // 20
+console.log(teaInstances[0].describe());
+// "Sencha (green) from Japan - 12.00 DKK/100g [organic]"
+console.log(teaInstances[1].describe());
+// "Earl Grey (black) from India - 8.00 DKK/100g"
