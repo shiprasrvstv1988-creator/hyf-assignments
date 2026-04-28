9d17149dc75afc4d949c6744a7b6a56045fa0d7f
Shipra Srivastava <shiprasrvstv1988@gmail.com>
2026-04-28 21:09:25 +0200

    Week 1 Node.js search engine + snippets API

---
+++ courses/backend/nodejs/week1/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import snippetsRouter from "./api/src/routers/snippets.js";
+import tagsRouter from "./api/src/routers/tags.js";
+import searchRouter from "./api/src/routers/search.js";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use("/api/snippets", snippetsRouter);
+app.use("/api/tags", tagsRouter);
+app.use("/search", searchRouter);
+
+app.get("/", (req, res) => {
+  res.send("Hello Class!");
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
